7f145af11c2c1d26187a6a79991ca1f9ea8293b6
jkhoang313 <jkhoang313@gmail.com>
2017-01-31 18:51:21 -0500

    Fixed Wild Pokemon percentages

--- app/models/pokedex.rb
+++ app/models/pokedex.rb
@@ -16,36 +16,51 @@ class Pokedex < ApplicationRecord
   end
 
   def self.starter_pokemon(choice)
-    if choice == 0
-      self.random_starter
-    else
-      Pokedex.find(choice)
-    end
+    # if choice == 0
+    #   self.random_starter
+    # else
+    #   Pokedex.find(choice)
+    # end
+    Pokedex.find(choice)
   end
-
-  def self.random_starter
-    common = ["Caterpie", "Weedle", "Pidgey", "Rattata", "Spearow", "Zubat", "Tentacool", "Geodude", "Magikarp"]
-    rare = ["Ekans", "Sandshrew", "Nidoran-f", "Nidoran-m", "Oddish", "Paras", "Venonat", "Diglett", "Meowth", "Mankey", "Abra", "Machop", "Bellsprout", "Magnemite", "Doduo", "Grimer", "Shellder", "Voltorb", "Koffing", "Tangela", "Horsea", "Goldeen", "Staryu"]
-    super_rare = ["Pikachu", "Clefairy", "Vulpix", "Jigglypuff", "Psyduck", "Growlithe", "Poliwag", "Ponyta", "Slowpoke", "Farfetchd", "Seel", "Gastly", "Onix", "Drowzee", "Krabby", "Exeggcute", "Cubone", "Rhyhorn", "Chansey", "Tauros", "Ditto", "Eevee"]
-    ultra_rare = ["Pinsir", "Lickitung", "Hitmonlee", "Hitmonchan", "Kangaskhan", "Lapras", "Mr-mime", "Scyther", "Jinx", "Electabuzz", "Magmar", "Porygon", "Omanyte", "Kabuto", "Aerodactyl", "Snorlax", "Dratini"]
-    #make better percentages
-
-    random_number = rand(1..100)
-
-    if random_number < 40
-      Pokedex.find_by(name: common.sample)
-    elsif random_number < 70
-      Pokedex.find_by(name: rare.sample)
-    elsif random_number < 90
-      Pokedex.find_by(name: super_rare.sample)
-    else
-      Pokedex.find_by(name: ultra_rare.sample)
-    end
-  end
+  #
+  # def self.random_starter
+  #   common = ["Caterpie", "Weedle", "Pidgey", "Rattata", "Spearow", "Zubat", "Tentacool", "Geodude", "Magikarp"]
+  #   rare = ["Ekans", "Sandshrew", "Nidoran-f", "Nidoran-m", "Oddish", "Paras", "Venonat", "Diglett", "Meowth", "Mankey", "Abra", "Machop", "Bellsprout", "Magnemite", "Doduo", "Grimer", "Shellder", "Voltorb", "Koffing", "Tangela", "Horsea", "Goldeen", "Staryu"]
+  #   super_rare = ["Pikachu", "Clefairy", "Vulpix", "Jigglypuff", "Psyduck", "Growlithe", "Poliwag", "Ponyta", "Slowpoke", "Farfetchd", "Seel", "Gastly", "Onix", "Drowzee", "Krabby", "Exeggcute", "Cubone", "Rhyhorn", "Chansey", "Tauros", "Ditto", "Eevee"]
+  #   ultra_rare = ["Pinsir", "Lickitung", "Hitmonlee", "Hitmonchan", "Kangaskhan", "Lapras", "Mr-mime", "Scyther", "Jinx", "Electabuzz", "Magmar", "Porygon", "Omanyte", "Kabuto", "Aerodactyl", "Snorlax", "Dratini"]
+  #   #make better percentages
+  #
+  #   random_number = rand(1..100)
+  #
+  #   if random_number < 40
+  #     Pokedex.find_by(name: common.sample)
+  #   elsif random_number < 70
+  #     Pokedex.find_by(name: rare.sample)
+  #   elsif random_number < 90
+  #     Pokedex.find_by(name: super_rare.sample)
+  #   else
+  #     Pokedex.find_by(name: ultra_rare.sample)
+  #   end
+  # end
 
   def self.wild
-    roster = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran-f", "Nidorina", "Nidoqueen", "Nidoran-m", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetchd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr-mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew"]
-    roster.sample
+    common = ["Caterpie", "Weedle", "Pidgey", "Rattata", "Spearow", "Ekans", "Magikarp", "Geodude", "Tentacool"]
+    rare = ["Ditto", "Hitmonlee", "Hitmonchan", "Onix", "Tangela", "Chansey", "Horsea", "Goldeen", "Staryu", "Pikachu", "Sandshrew", "Nidoran-f", "Nidoran-m", "Clefairy", "Vulpix", "Jigglypuff", "Zubat", "Oddish", "Paras", "Venonat", "Diglett", "Meowth", "Psyduck", "Mankey", "Growlithe", "Poliwag", "Abra", "Machop", "Bellsprout",   "Ponyta", "Slowpoke", "Magnemite", "Farfetchd", "Doduo", "Seel", "Grimer", "Shellder", "Gastly",  "Drowzee", "Krabby", "Voltorb", "Exeggcute", "Cubone",  "Lickitung", "Koffing", "Porygon", "Omanyte", "Kabuto"]
+    super_rare = ["Dratini", "Mr-mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Aerodactyl", "Snorlax", "Tauros", "Lapras", "Kangaskhan", "Eevee", "Rhyhorn"]
+    ultra_rare = ["Articuno", "Zapdos", "Moltres", "Mewtwo", "Mew"]
+
+    random_number = rand(1..1000)
+
+    if random_number < 400
+      common.sample
+    elsif random_number < 900
+      rare.sample
+    elsif random_number < 999
+      super_rare.sample
+    else
+      ultra_rare.sample
+    end
   end
 
   def fixed_weakness
