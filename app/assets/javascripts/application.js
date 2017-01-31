e95399f8556403d21ca68d6bb184854d85e436e1
jkhoang313 <jkhoang313@gmail.com>
2017-01-31 17:31:03 -0500

    Added pokeballs images to capture and experimented with side nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,8 @@
 $(document).ready(function() {
     $('select').material_select()
     $(".dropdown-button").dropdown();
+    // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
 });
